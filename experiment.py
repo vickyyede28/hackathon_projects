ccbd77a409aeda60609f107f3f2fbe595e5d1c63
vickyyede28 <92183912+vickyyede28@users.noreply.github.com>
2021-10-19 09:53:20 +0530

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,18 @@
+# table 
+# a= int(input())
+# b=1
+# while b<=10:
+#         print (a,'x',b ,'=',(a*b))
+#         b= b+1
+
+# table till user input
+# a=int(input())
+# b=2
+# while b<=a:
+#         c=1
+#         while c<=10:
+#                 print(b,'x',c,'=',(b*c))
+#                 c+=1
+#         b=b+1
+# print()
+        
