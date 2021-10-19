19c23186cdcd124a50d7183eb85637954763afb0
vickyyede28 <92183912+vickyyede28@users.noreply.github.com>
2021-10-19 09:54:26 +0530

    Add files via upload

---
+++ all question.py
@@ -0,0 +1,211 @@
+# i=0
+# n=int(input('enter your no'))
+# while i<n:
+#     i=i+1
+#     print(i)
+
+# i=20
+# while i<=40:
+#     if i%2==0:
+#         print(i)
+#     i=i+1
+
+# i=1
+# sum=0
+# while (i<=100):
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+
+
+# i=1
+# sum=0
+# while i<=10:
+#     x=int(input("enter number"))
+#     sum=sum+x
+#     i=i+1
+# print(sum)
+
+
+# i=1
+# while i<=100:
+#     if i%2==0:
+#         print(i)
+#     else:
+#         print(-i)
+#     i=i+1
+
+# i=891
+# while i<931:
+#     z=i-890
+#     if z%3==0:
+#         print(z)
+#     i=i+1
+
+
+# i=0
+# while i<=50:
+#     if i!=0:
+#         print(i)
+#     i=i+5
+
+# count=556
+# while count<656:
+#     x=count-555
+#     print(x)
+#     count=count+1
+
+# i=1
+# while i<=7:
+#     print("*"*i)
+#     i=i+1
+
+# i=1
+# sum=0
+# while i<=10:
+#     y=int(input("enter the number"))
+#     sum=sum+y
+#     i=i+1
+# print(sum)
+
+
+# count=156
+# while count<166:
+#     x=count-155
+#     print(x)
+#     count=count+1
+
+
+# num=int(input("enter number"))
+# i=1
+# a=0
+# while i<=num:
+#     if num%i==0:
+#         a=a+1
+#     i=i+1
+# if a==2:
+#     print(num,"prime number")
+# else:
+#     print(num,"not prime number")
+
+
+# n=11
+# i=1
+# sum=0
+# avg=0
+# while i<=n:
+#     num=int(input("enter age"))
+#     sum=sum+num
+#     avg=sum/n
+#     i=i+1
+# print(avg)
+# if avg%5==0:
+#     print(avg,"divisible by 5")
+# else:
+#     print(avg,"not divisible by 5 ")
+
+
+# x=int(input("enter year"))
+# y=int(input("enter year"))
+# i=x
+# while i<=y:
+#     if i%4==0 and i%100!=0 or i%400==0:
+#         print(i,"leap year")
+#     else:
+#         print(i,"not leap year")
+#     i=i+1
+
+
+# x=int(input("enter number"))
+# i=x
+# while i<=x:
+#     print("#"*i)
+#     i=i+1
+
+
+# x=int(input("enter number"))
+# i=1
+# while i<=x:
+#     y=int(input("enter number"))
+#     print(x+y)
+#     i=i+1
+
+# n=int(input("enter number"))
+# i=1
+# while n<=1:
+#     i=i+1
+# print(n+i)
+
+# a= 10
+# sum= 0
+# while a>0:
+#     num= int(input())
+#     sum= sum + num
+#     a=a-1
+#     print('average value',sum/10.0)
+
+
+# a=1
+# while a<=4:
+#     print('*'* a)
+#     a=a+1
+
+
+# while True:
+    # print('infinity')
+
+
+# factorial
+
+# a= int(input())
+# fac=1
+# while a>=1:
+#     fac = fac*a
+#     a=a-1
+# print(fac)
+
+
+
+
+# a=['hello world','i am good']
+# count=0
+# for i in range (len (a)):
+#         a.replace()
+#         count = count+1
+# print([(len(a[0])),(len(a[1]))])
+
+
+
+# a= [10,12,31,23,45,50]
+# max=a[0]
+# for i in a:
+#     if i>max:
+#         max=i
+# print(max)
+
+
+
+# rows = int(input("Enter the number of rows: "))
+# for i in range(rows):
+#     for j in range (rows):
+#         print('*',end=' ')
+#     print()
+
+# a=int(input(''))
+# for i in range(len(a)):
+#     print(i)
+
+#######################################################################################
+# THE END               
+
+
+
+
+########################################################################
+#TABLE OF EVERY NO :
+# a=int(input())
+# for i in range(1,a+1):
+#     for s in range (1,11):
+#         print(i,'x',s,'=',i*s)
+#         print()
+###########################################################################
