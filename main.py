1fb35890a21fde09a7f6f7bb05c560c93d97a8df
vickyyede28 <92183912+vickyyede28@users.noreply.github.com>
2021-10-19 10:11:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+# # Q1
+# for i in range(1500,2700):
+#     if i%7==0 and i%5==0:
+#         print(i)
+
+
+
+# Fibonacci series
+
+# a=0
+# b=1
+# c=0
+# Counter=0
+# for i in range(1,12):
+#     print(c,end=' ')
+#     a=b
+#     b=c
+#     c=a+b
+# user1=int(input('enter your no'))
+# user2=int(input('enter your no'))
+# while c<0:
+#     if c%2==0:
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+#changing ascending to decending
+# a=[5,1,6,3,7,2]
+# for i in range(len(a)):
+#     for j in range(len(a)-1):
+#         if a[j]>a[j+1]:
+#             a[j],a[j+1]=a[j+1],a[j]
+#             print(a)
+
+#     print()
+# print(a)
+
+
+
+#changing ascending to decending
+# a=[5,1,6,3,7,2]
+# for i in range(len(a)):
+#     # print(i)
+#     for j in range(len(a)-1):
+#         print(a,a[j],a[j+1])
+#         if a[j]<a[j+1]:
+#             a[j],a[j+1]=a[j+1],a[j]
+            
+#     print()
+# print(a)
