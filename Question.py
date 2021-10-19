0fa494cd025c823383fa52919632284cea69fbd9
vickyyede28 <92183912+vickyyede28@users.noreply.github.com>
2021-10-19 09:29:01 +0530

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,106 @@
+# print('/\\/\\/\\/\\')
+# print('-|')
+# print("Hello\fWorld!")
+
+# print('___\f|'*1)
+# print('____\v|'*50) 
+
+# print('|_____|\n|     |\n'*6)
+
+# list = ['name','postion','salary']
+# print(list).reverse
+
+# str = ['Name','Position','Salary']
+# #convert string to list of chars
+# str_list = list(str)
+
+# #reverse the list
+# str_list.reverse()
+
+# #join the list items
+# reversed = ''.join(str_list)
+
+# #print reversed string
+# print(reversed)
+
+# a=input("enter")
+# b=a[::-1]
+# print(b)
+# if a==b:
+#     print("hai")
+# else:
+#     print("nahi hai")
+
+a='s,a,t,i,s,h'
+for i in a :
+    print(end='' + i)
+# # b=a.split(",")
+# # print(b)
+# s="sas"
+# print(s+"a"+"b")
+# # a = 'satish'
+# print( 's'+'a'+'t'+'i'+'s'+'h')
+
+###################################################################
+
+# print('Which shape do you want to print 2D OR 3D')
+# shape= int(input('Choose your shape'))
+# print("""
+#     1. Rectangle
+#     2. Square
+#     3. Rhombus""")
+# l = int(input('Length: '))
+# b = int(input('Breadth: '))
+# angle=int(input('One angle of the Quadrilateral: '))
+# if l==b and angle==90:
+#         print('Square')
+# elif l!=b and angle==90:
+#         print('Rectangle')
+# elif l==b:
+#     print('Rhombus')
+# else:
+#     print('Nothing.')
+
+# a= 'hello navgurukul'
+# l = len(a)
+# h = l//2
+# print(a[:h])
+# print(a[h:])
+
+    
+
+# a= int(input('what is the lenght'))
+# b= int(input('what is the breath'))
+
+
+
+# for i  in range (1,6) :
+#         print ('*'*i)
+
+# a='v','i','c','k','y'
+# for i in a :
+#     print(end='' + i)
+# # b=a.split(",")
+# # print(b)
+# s="sas"
+# print(s+"a"+"b")
+# # a = 'satish'
+# print( 's'+'a'+'t'+'i'+'s'+'h')
+
+
+# whi
+# a= input()
+# st=""
+# for i in a :
+#     st+=i+","
+# print(st[:-1])
+
+# a= input()
+# st= ""
+# for i in a :
+#     st+=i+','
+# print(st[:-1])
+a="abcdefghijklmnopqrstuvwxyz"
+b=a[::-1]
+print(b)
+
